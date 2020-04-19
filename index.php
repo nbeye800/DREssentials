d7dc35cf649e9e69f8ecb4a2a158a7ba74b835ee
nbeye800 <44952820+nbeye800@users.noreply.github.com>
2020-04-18 22:04:54 -0400

    creating landing page

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/* Created by Nichole Beyer
+ * Course: CSC354 Spring 2020
+ * Assignment: DR Essentials
+ * Document description: landing page with buttons to log in
+*/
+
+/* create and include a functions.php file */
+/* Start session here */
+
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <title>DR Essentials</title>
+   <meta charset="utf-8">
+   <!-- insert a link to stylesheet when made -->
+ </head>
+ <body>
+   <!-- insert logo -->
+
+   <!-- log in form -->
+   <div id='id01' class="login">
+     <!-- creating an x button to close form -->
+     <span onclick="closeform();" class="close" title="Close Sign In">&times;</span>
+     <form class="SignInContent" action="index.php" method="post">
+      <div class="container">
+        <h2>Sign In</h2>
+          <p> Please enter your credidentials to login to DR Essentials</p>
+          <!--Horizontal rule-->
+
+</body>
