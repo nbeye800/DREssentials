ae2983a1278029137bbbd11618345f6bd3a9a6a8
Nichole Beyer <nbeye800@live.kutztown.edu>
2020-04-18 23:45:20 -0400

    edited

--- index.php
+++ index.php
@@ -21,14 +21,44 @@
  <body>
    <!-- insert logo -->
 
+   <!--make button-->
+  <button class='btn' type='button' onclick='openForm();'>Login</button>
+
    <!-- log in form -->
    <div id='id01' class="login">
      <!-- creating an x button to close form -->
-     <span onclick="closeform();" class="close" title="Close Sign In">&times;</span>
-     <form class="SignInContent" action="index.php" method="post">
+     <span onclick="closeForm();" class="close" title="Close Sign In">&times;</span>
+     <form class="signInContent" action="index.php" method="post">
       <div class="container">
         <h2>Sign In</h2>
           <p> Please enter your credidentials to login to DR Essentials</p>
           <!--Horizontal rule-->
+          <hr>
+          <label for="email_1"><b>Email</b></label>
+          <input type="email" id="email_1" placeholder="Enter Your KU Email" name="email" required>
 
+          <label for="password_1"><b>Password</b></label>
+          <input type="password" id="password_1" placeholder="Enter Your KU Password" name="password" required>
+
+          <input type="hidden" name="action" value="signIn">
+
+          <!--Cancel and Submit Buttons-->
+          <button type="button" onclick="closeform();" class="cancelbtn">Cancel</button>
+          <button type="submit" class="signinbtn">Sign In</button>
+          <br><br><br>
+      </div>
+    </form>
+  </div>
+  <!--Functions for Form-->
+  <script>
+    function openForm(){
+      document.getElementById('id01').style.display = "block";
+    }
+  </script>
+  <script>
+    function closeForm(){
+      document.getElementById('id01').style.display = "none";
+    }
+  </script>
 </body>
+</html>
