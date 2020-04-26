80842d0a780fd9436433763a04cbb40a3614a127
nbeye800 <44952820+nbeye800@users.noreply.github.com>
2020-04-25 23:58:22 -0400

    getUserRecord

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+// Edited By: Nichole Beyer DR Essentials CSC354 Spring2020
+// History of function creation
+// Edited By: Nichole Beyer Final Project CSC242 Fall 2018
+// Created By: Dr. Schwesinger, CSC 242, Fall 2018, Assignment 9
+
+/* Function Name: getUserRecord
+ * Description: get user information from the database
+ * Parameters: (string) $email: the user's email
+ *             (string) $password: the user's password
+ * Return Value: (array) The user's record if it exists, otherwise an empty
+ *               array
+ */
+ 
+ function getUserRecord($email, $password) {
+
+    // try to insert into the database
+    // if an error occurs return FALSE
+    try {
+        $db =  new PDO("sqlite:users.db");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM ACCOUNT_INFO WHERE EMAIL='$email' and PASSWORD='$password'";
+        $stmt = $db->query($sql);
+        // there should only be a single record
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e) {
+        print "<p>$e</p>";
+        return array();
+    }
+}
+
+?>
