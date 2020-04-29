b4b34020493c9cd845f5bf7a9c5b41f4c74e8c42
nbeye800 <44952820+nbeye800@users.noreply.github.com>
2020-04-29 04:10:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if(!$a){
    <!--make button-->
  
   <div id='id01' class="signIn">
-   <form class="signIn" action="location.href='landing_page.html';" method="post">
+   <form class="signIn" action="landing_page.html" method="post">
    <div class="container">
     <label for="email_1"><b>Email</b></label>
     <input type="email" id="email_1" placeholder="Enter Your KU Email" name="email" required>
